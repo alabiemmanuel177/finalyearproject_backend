38554b512e0dbc2ff4f052f9a23751b97b4d67cf
Alabi <alabiemmanuel177@gmail.com>
2023-02-02 18:46:14 +0100

    ./groups

--- routes/Group.js
+++ routes/Group.js
@@ -1,75 +1,72 @@
 const router = require("express").Router();
 const Group = require("../models/Group");
 
-//CREATE ASSIGNMENT ANSWER
+//CREATE GROUP
 router.post("/", async (req, res) => {
-  const newAssignmentAnswer = new AssignmentAnswer(req.body);
+  const newGroup = new Group(req.body);
   try {
-    const savedAssignmentAnswer = await newAssignmentAnswer.save();
-    return res.status(200).json(savedAssignmentAnswer);
+    const savedGroup = await newGroup.save();
+    return res.status(200).json(savedGroup);
   } catch (err) {
     return res.status(500).json(err);
   }
 });
 
-//UPDATE ASSIGNMENT ANSWER
+//UPDATE GROUP
 router.put("/:id", async (req, res) => {
   try {
-    const updatedAssignmentAnswer = await AssignmentAnswer.findByIdAndUpdate(
+    const updatedGroup = await Group.findByIdAndUpdate(
       req.params.id,
       {
         $set: req.body,
       },
       { new: true }
     );
-    return res.status(200).json(updatedAssignmentAnswer);
+    return res.status(200).json(updatedGroup);
   } catch (err) {
     return res.status(500).json(err);
   }
 });
 
-//DELETE ASSIGNMENT ANSWER
+//DELETE GROUP
 router.delete("/:id", async (req, res) => {
   try {
-    const assignmentAnswer = await AssignmentAnswer.findById(req.params.id);
-    await assignmentAnswer.delete();
-    return res.status(200).json("Assignment Answer has been deleted...");
+    const group = await Group.findById(req.params.id);
+    await group.delete();
+    return res.status(200).json("Group Answer has been deleted...");
   } catch (err) {
     return res.status(500).json(err);
   }
 });
 
-//GET ASSIGNMENT ANSWER
+//GET GROUP
 router.get("/:id", async (req, res) => {
   try {
-    const assignmentAnswer = await AssignmentAnswer.findById(req.params.id);
-    return res.status(200).json(assignmentAnswer);
+    const group = await Group.findById(req.params.id);
+    return res.status(200).json(group);
   } catch (err) {
     return res.status(500).json(err);
   }
 });
 
-//GET ALL ASSIGNMENT ANSWER
+//GET ALL GROUP
 router.get("/", async (req, res) => {
   try {
-    let assignmentAnswers;
-    assignmentAnswers = await AssignmentAnswer.find();
-    return res.status(200).json(assignmentAnswers);
+    let groups;
+    groups = await Group.find();
+    return res.status(200).json(groups);
   } catch (err) {
     return res.status(500).json(err);
   }
 });
 
-//PATCH ASSIGNMENT ANSWER
+//PATCH GROUP
 router.patch("/:id", async (req, res) => {
   try {
-    const updatedAssignmentAnswer = await AssignmentAnswer.findByIdAndUpdate(
-      req.params.id,
-      {
-        $push: req.body,
-      }
-    );
-    return res.status(200).json(updatedAssignmentAnswer);
+    const group = await Group.findByIdAndUpdate(req.params.id, {
+      $push: req.body,
+    });
+    return res.status(200).json(group);
   } catch (err) {
     return res.status(500).json(err);
   }
